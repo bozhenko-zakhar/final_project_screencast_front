681730961536cfe703b06057a93c45cac9cfd116
Bozhenko Zakhar <zakhar1bozhenko@gmail.com>
2026-04-27 09:09:49 +0200

    Init next project

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+// "use client"
+
+import css from "./loading.module.css"
+
+const Loading = () => {
+	return (
+		<p className={css.text}>Loading, please wait...</p>
+	)
+};
+
+export default Loading;
