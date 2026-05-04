4e3d668e9e911393c012d317b0c70d0b1313f852
OlenaKarpova <kal4enko@gmail.com>
2026-05-04 19:21:01 +0200

    my changes in new branch from main

--- components/DashBoardPage/DashboardPage_main/FeelingCheckCard/FeelingCheckCard.tsx
+++ components/DashBoardPage/DashboardPage_main/FeelingCheckCard/FeelingCheckCard.tsx
@@ -7,18 +7,14 @@ import styles from "./FeelingCheckCard.module.css";
 import cardStyles from "../../DashboardPage_main/DashboardPage_main.module.css";
 
 import { useAuthStore } from "@/lib/store/authStore";
-// взяти у команди:
-// import AddDiaryEntryModal from '@components/AddDiaryEntryModal/AddDiaryEntryModal';
-import UserFeelingContent from "./UserFeelingContent";
+import DiaryEntryModal from "@/components/AddDiaryEntryModal/AddDiaryEntryModal";
+import { DiaryEntryForm } from "@/components/AddDiaryEntryForm/AddDiaryEntryForm";
 
 const FeelingCheckCard = () => {
   const router = useRouter();
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
-  const user = useAuthStore((state) => state.user);
-
-  console.log("Поточний тиждень:", 1); // видалити
 
   const handleOpenModal = () => {
     if (!isAuthenticated) {
@@ -28,31 +24,31 @@ const FeelingCheckCard = () => {
     setIsModalOpen(true);
   };
 
+  const handleCloseModal = () => setIsModalOpen(false);
+
   return (
-    <> 
+    <>
       <section
         className={`${cardStyles.card} ${cardStyles.cardFixedHeight} ${styles.feeling}`}
       >
-        {isAuthenticated ? (
-        <UserFeelingContent weekNumber = {1} />
-        ) : (
-            <>
-              <h2 className={styles.title}>Як ви себе почуваєте?</h2>
-              <p className={styles.subtitle}>Рекомендація на сьогодні:</p>
-              <p className={styles.text}>Занотуйте незвичні відчуття у тілі.</p>
-              <button type="button" className={styles.button} onClick={handleOpenModal}>
-                Зробити запис у щоденник
-              </button>
-            </>
-        )}
+        <h2 className={styles.title}>Як ви себе почуваєте?</h2>
+        <p className={styles.subtitle}>Рекомендація на сьогодні:</p>
+        <p className={styles.text}>Занотуйте незвичні відчуття у тілі.</p>
+        <button
+          type="button"
+          className={styles.button}
+          onClick={handleOpenModal}
+        >
+          Зробити запис у щоденник
+        </button>
       </section>
-      {/* {isAuthenticated && (
-        <AddDiaryEntryModal
-          isOpen={isModalOpen}
-          onClose={() => setIsModalOpen(false)}
-        />
-          )} */}
-      </>
+
+      {isModalOpen && (
+        <DiaryEntryModal onClose={handleCloseModal} title="Новий запис">
+          <DiaryEntryForm onClose={handleCloseModal} />
+        </DiaryEntryModal>
+      )}
+    </>
   );
 };
 
