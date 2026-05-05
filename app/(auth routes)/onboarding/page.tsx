1a12d74b8edcce8b1ee3e9c7f29fd3b5f2cc1f1d
Oleksii Cherep <alex1998120850@gmail.com>
2026-05-05 17:34:08 +0300

    onboarding

---
+++ app/(auth routes)/onboarding/page.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+import OnboardingForm from '@/components/OnboardingForm/OnboardingForm';
+import styles from './page.module.css';
+
+export default function OnboardingPage() {
+  return (
+    <main className={styles.container}>
+      <Image 
+        src="/Plant.jpg" 
+        alt="Plant Decoration" 
+        width={150} 
+        height={150} 
+        className={styles.plantImage}
+        priority
+      />
+
+      <div className={styles.header}>
+        <h1 className={styles.title}>Давайте знайомитись ближче</h1>
+      </div>
+
+      <OnboardingForm />
+    </main>
+  );
+}
