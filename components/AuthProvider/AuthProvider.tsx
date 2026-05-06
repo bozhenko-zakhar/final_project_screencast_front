acc133e7e51aa43524e630e021520ce5510c29aa
Bozhenko Zakhar <zakhar1bozhenko@gmail.com>
2026-05-06 17:29:35 +0200

    Update AuthProvider.tsx

--- components/AuthProvider/AuthProvider.tsx
+++ components/AuthProvider/AuthProvider.tsx
@@ -16,7 +16,7 @@ export const AuthProvider = ({ children }: Props) => {
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        const user: User = await getMe();
+        const user = await getMe();
         setUser(user);
       } catch (error) {
         clearUser();
