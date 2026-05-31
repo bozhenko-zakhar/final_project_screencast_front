e3176a2cbcd6cba54e80b75af2eff6ab4dea4bd4
Bozhenko Zakhar <zakhar1bozhenko@gmail.com>
2026-05-31 19:17:47 +0200

    Fix styles in RegisterForm component

--- app/(auth routes)/auth/register/page.tsx
+++ app/(auth routes)/auth/register/page.tsx
@@ -9,7 +9,7 @@ export default function Register() {
   return (
     <main>
       <div className={clsx(css.box, 'container')}>
-        <div className={css.formBox}>
+				<div className={css.formBox}>
           <RegisterForm />
         </div>
         <div className={css.imageBox}>
@@ -17,12 +17,12 @@ export default function Register() {
             src="/image/register.png"
             alt="eggs"
             fill
-          className={css.image}
-          sizes="(min-width: 1440px) 50vw, 0px"
-          priority
+						className={css.image}
+						sizes="(min-width: 1440px) 50vw, 0px"
+						priority
           />
-          </div>
-          </div>
+				</div>
+			</div>
     </main>
   );
 }
