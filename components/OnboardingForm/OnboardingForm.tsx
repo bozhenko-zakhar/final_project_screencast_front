4d421443dc56e008a9f698af95243822a7d3af16
Bozhenko Zakhar <zakhar1bozhenko@gmail.com>
2026-06-04 15:14:08 +0200

    Finish styling in Onboarding component

--- components/OnboardingForm/OnboardingForm.tsx
+++ components/OnboardingForm/OnboardingForm.tsx
@@ -94,139 +94,142 @@ export default function OnboardingForm({ user }: Props) {
 				</svg>
 			</Link>
 
-			<h2 className={css.title}>Давайте познаймимось ближче</h2>
-			
-			<div>
-				<Image
-					src="/image/Avatar-def.jpg"
-					alt="Plant Decoration"
-					className={css.image}
-					width={164}
-					height={164}
-				/>
+			<div className={css.form_container}>
 
-				<Button className={css.photo_btn}>Завантажити фото</Button>
-			</div>
+				<h2 className={css.title}>Давайте познаймимось ближче</h2>
+				
+				<div>
+					<Image
+						src="/image/Avatar-def.jpg"
+						alt="Plant Decoration"
+						className={css.image}
+						width={164}
+						height={164}
+					/>
 
-			<Formik<FormValues>
-				initialValues={{
-					gender: "",
-					dueDate: user.dueDate ? user.dueDate.split("T")[0] : "",
-				}}
-				validationSchema={validationSchema}
-				enableReinitialize
-				onSubmit={(values, { resetForm }) => {
-					const payload: Partial<UpdateUserPayload> & {
-						name?: string;
-						date?: string | null;
-						newEmail?: string;
-					} = {};
+					<Button className={css.photo_btn}>Завантажити фото</Button>
+				</div>
 
-					if (
-						values.dueDate !== (user.dueDate ? user.dueDate.split("T")[0] : "")
-					) {
-						payload.date = values.dueDate || null;
-					}
+				<Formik<FormValues>
+					initialValues={{
+						gender: "",
+						dueDate: user.dueDate ? user.dueDate.split("T")[0] : "",
+					}}
+					validationSchema={validationSchema}
+					enableReinitialize
+					onSubmit={(values, { resetForm }) => {
+						const payload: Partial<UpdateUserPayload> & {
+							name?: string;
+							date?: string | null;
+							newEmail?: string;
+						} = {};
 
-					if (values.gender !== (user.gender || "")) {
-						payload.gender = values.gender || null;
-					}
+						if (
+							values.dueDate !== (user.dueDate ? user.dueDate.split("T")[0] : "")
+						) {
+							payload.date = values.dueDate || null;
+						}
 
-					mutate(payload as UpdateUserPayload, {
-						onSuccess: async (updatedUser) => {
-							setUser(updatedUser);
-							queryClient.setQueryData(["user"], updatedUser);
+						if (values.gender !== (user.gender || "")) {
+							payload.gender = values.gender || null;
+						}
 
-							document.body.dataset.theme =
-								updatedUser.gender === "girl" || updatedUser.gender === "boy" ?
-									updatedUser.gender
-								: "neutral";
+						mutate(payload as UpdateUserPayload, {
+							onSuccess: async (updatedUser) => {
+								setUser(updatedUser);
+								queryClient.setQueryData(["user"], updatedUser);
 
-							toast.success("Профіль збережено");
+								document.body.dataset.theme =
+									updatedUser.gender === "girl" || updatedUser.gender === "boy" ?
+										updatedUser.gender
+									: "neutral";
 
-							resetForm({
-								values: {
-									gender: updatedUser.gender || "",
-									dueDate:
-										updatedUser.dueDate ? updatedUser.dueDate.split("T")[0] : "",
-								},
-							});
-						},
+								toast.success("Профіль збережено");
 
-						onError: () => {
-							toast.error("Не зберегти оновити профіль");
-						},
-					});
-				}}
-			>
-			{() => (
-				<Form className={css.form}>
-					<div className={css.fields}>
-						<label className={css.label}>
-							<span>Стать дитини</span>
-							<div className={css.inputWrapper}>
-								<Field name="gender">
-									{({ field, form }: FieldProps<string, FormValues>) => (
-									<Select<GenderOption, false>
-										unstyled
-										options={genderOptions}
-										value={genderOptions.find(
-											(option) => option.value === field.value,
+								resetForm({
+									values: {
+										gender: updatedUser.gender || "",
+										dueDate:
+											updatedUser.dueDate ? updatedUser.dueDate.split("T")[0] : "",
+									},
+								});
+							},
+
+							onError: () => {
+								toast.error("Не зберегти оновити профіль");
+							},
+						});
+					}}
+				>
+				{() => (
+					<Form className={css.form}>
+						<div className={css.fields}>
+							<label className={css.label}>
+								<span>Стать дитини</span>
+								<div className={css.inputWrapper}>
+									<Field name="gender">
+										{({ field, form }: FieldProps<string, FormValues>) => (
+										<Select<GenderOption, false>
+											unstyled
+											options={genderOptions}
+											value={genderOptions.find(
+												(option) => option.value === field.value,
+											)}
+											onChange={(option: SingleValue<GenderOption>) => {
+												const gender = option?.value || "";
+
+												form.setFieldValue("gender", gender);
+
+												document.body.dataset.theme =
+													gender === "girl" || gender === "boy" ?
+														gender
+													: "neutral";
+											}}
+											onBlur={() => form.setFieldTouched("gender", true)}
+											placeholder="Оберіть стать"
+											isSearchable={false}
+											classNames={selectClassNames}
+											components={{
+												DropdownIndicator,
+												IndicatorSeparator: () => null,
+											}}
+										/>
 										)}
-										onChange={(option: SingleValue<GenderOption>) => {
-											const gender = option?.value || "";
+									</Field>
+								</div>
+							</label>
 
-											form.setFieldValue("gender", gender);
-
-											document.body.dataset.theme =
-												gender === "girl" || gender === "boy" ?
-													gender
-												: "neutral";
-										}}
-										onBlur={() => form.setFieldTouched("gender", true)}
-										placeholder="Оберіть стать"
-										isSearchable={false}
-										classNames={selectClassNames}
-										components={{
-											DropdownIndicator,
-											IndicatorSeparator: () => null,
-										}}
-									/>
-									)}
-								</Field>
-							</div>
-						</label>
-
-						<label className={css.label}>
-							<span>Планова дата пологів</span>
-							<div className={css.inputWrapper}>
-								<Field name="dueDate">
-									{({ field, form }: FieldProps<string, FormValues>) => (
-									<CalendarPicker
-										id="dueDate"
-										value={field.value}
-										placeholder="Оберіть дату"
-										disabled={isPending}
-										onChange={(date) => {
-											form.setFieldValue("dueDate", date);
-											form.setFieldTouched("dueDate", true, false);
-										}}
-									/>
-									)}
-								</Field>
-							</div>
-							<ErrorMessage
-								name="dueDate"
-								component="p"
-								className={css.error}
-							/>
-						</label>
-					</div>
-					
-					<Button>Зберегти</Button>
-				</Form>
-				)}
-			</Formik>
+							<label className={css.label}>
+								<span>Планова дата пологів</span>
+								<div className={css.inputWrapper}>
+									<Field name="dueDate">
+										{({ field, form }: FieldProps<string, FormValues>) => (
+										<CalendarPicker
+											id="dueDate"
+											value={field.value}
+											placeholder="Оберіть дату"
+											disabled={isPending}
+											onChange={(date) => {
+												form.setFieldValue("dueDate", date);
+												form.setFieldTouched("dueDate", true, false);
+											}}
+										/>
+										)}
+									</Field>
+								</div>
+								<ErrorMessage
+									name="dueDate"
+									component="p"
+									className={css.error}
+								/>
+							</label>
+						</div>
+						
+						<Button>Зберегти</Button>
+					</Form>
+					)}
+				</Formik>
+			</div>
 		</div>
 	);
 };
