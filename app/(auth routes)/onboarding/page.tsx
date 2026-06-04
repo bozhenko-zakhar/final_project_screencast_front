31a7a00ae988f5ed433a6d40a069cbd978f31c39
Bozhenko Zakhar <zakhar1bozhenko@gmail.com>
2026-06-04 15:16:56 +0200

    Revert "Clearing and starting/onboarding"

--- app/(auth routes)/onboarding/page.tsx
+++ app/(auth routes)/onboarding/page.tsx
@@ -1,37 +1,24 @@
 import Image from 'next/image';
-
 import OnboardingForm from '@/components/OnboardingForm/OnboardingForm';
+import styles from './page.module.css';
 
-import css from './page.module.css';
-import { redirect } from 'next/navigation';
-import { User } from '@/types/user';
-import { getServerUser } from '@/lib/api/serverApi/users';
-
-export default async function OnboardingPage() {
-	let user: User;
-
-	try {
-		user = await getServerUser();
-	} catch {
-		redirect("/auth/login");
-	}
-	
+export default function OnboardingPage() {
   return (
-		<section className={css.container}>
-			<div className={css.form_box}>
-				<OnboardingForm user={user} />
-			</div>
+    <main className={styles.container}>
+      <Image 
+        src="/Plant.jpg" 
+        alt="Plant Decoration" 
+        width={200} 
+        height={300}
+        className={styles.plantImage}
+        priority
+      />
 
-			<div className={css.image_box}>
-				<Image
-					src="/image/Plant.jpg"
-					alt="Plant Decoration"
-					fill
-					className={css.image}
-					sizes="(min-width: 1440px) 50vw, 0px"
-					priority
-				/>
-			</div>
-    </section>
+      <div className={styles.header}>
+        <h2 className={styles.title}>  <span className={styles.titleLine}>Давайте</span>  <span className={styles.titleLine}>познайомимось ближче</span></h2>
+      </div>
+
+      <OnboardingForm />
+    </main>
   );
 };
