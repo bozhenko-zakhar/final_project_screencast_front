5ae3200e2218bad69c24e50c7ec972a8c02d1475
Viacheslav-Bo <zotikivna@gmail.com>
2026-06-06 14:06:51 +0300

    fix type

--- lib/api/clientApi/diaries.ts
+++ lib/api/clientApi/diaries.ts
@@ -1,9 +1,9 @@
 import { nextServer } from "../api";
-import { 
-  DiaryEntry, 
-  BackendDiaryEntry, 
-  DiaryListItem, 
-  DiaryEntryDetail 
+import {
+  DiaryEntry,
+  BackendDiaryEntry,
+  DiaryListItem,
+  DiaryEntryDetail,
 } from "@/types/diary";
 
 const transformBackendEntry = (entry: BackendDiaryEntry): DiaryEntryDetail => ({
@@ -11,30 +11,39 @@ const transformBackendEntry = (entry: BackendDiaryEntry): DiaryEntryDetail => ({
   title: entry.title,
   date: entry.date,
   description: entry.description,
-  emotions: entry.emotions.map(emo => ({
-    id: emo._id.$oid,
+  emotions: entry.emotions.map((emo) => ({
+    _id: emo._id.$oid,
     title: emo.title,
   })),
 });
 
-export const createDiaryEntry = async (payload: DiaryEntry): Promise<DiaryEntry> => {
-  const response = await nextServer.post<BackendDiaryEntry>("/diaries", payload);
+export const createDiaryEntry = async (
+  payload: DiaryEntry,
+): Promise<DiaryEntry> => {
+  const response = await nextServer.post<BackendDiaryEntry>(
+    "/diaries",
+    payload,
+  );
   return response.data.data;
 };
 
 export const fetchDiaries = async (): Promise<DiaryListItem[]> => {
   const response = await nextServer.get("/diaries");
-  return response.data.data ;
+  return response.data.data;
 };
 
-export const getDiaryEntry = async (entryId: string): Promise<DiaryEntryDetail> => {
-  const response = await nextServer.get<BackendDiaryEntry>(`/diaries/${entryId}`);
+export const getDiaryEntry = async (
+  entryId: string,
+): Promise<DiaryEntryDetail> => {
+  const response = await nextServer.get<BackendDiaryEntry>(
+    `/diaries/${entryId}`,
+  );
   return transformBackendEntry(response.data);
 };
 
 export const updateDiaryEntry = async (
   entryId: string,
-  payload: Partial<DiaryEntry>
+  payload: Partial<DiaryEntry>,
 ): Promise<void> => {
   await nextServer.patch(`/diaries/${entryId}`, payload);
 };
