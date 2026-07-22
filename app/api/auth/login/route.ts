6b941377b04beb3f5bf297ddd5da4210d014f376
Viacheslav-Bo <zotikivna@gmail.com>
2026-07-22 15:18:50 +0300

    fix reload when password !==user.pasword

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -5,6 +5,7 @@ import { isAxiosError } from "axios";
 import { parse } from "cookie";
 
 export async function POST(req: NextRequest) {
+  console.log("LOGIN ROUTE HIT");
   try {
     const body = await req.json();
     const apiRes = await api.post("/auth/login", body);
@@ -40,19 +41,27 @@ export async function POST(req: NextRequest) {
 
     return NextResponse.json(apiRes.data, { status: apiRes.status });
   } catch (error) {
+    console.log("AXIOS ERROR");
+    console.log(error);
+
     if (isAxiosError(error)) {
+      console.log("STATUS:", error.response?.status);
+      console.log("DATA:", error.response?.data);
+
       return NextResponse.json(
         {
           error: error.message,
           response: error.response?.data,
         },
-        { status: error.response?.status || 500 }
+        { status: error.response?.status || 500 },
       );
     }
 
-    return NextResponse.json(
-      { error: "Internal Server Error" },
-      { status: 500 }
-    );
+    throw error;
   }
+
+  // return NextResponse.json(
+  //   { error: "Internal Server Error" },
+  //   { status: 500 }
+  // );
 }
